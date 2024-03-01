1c001ea50e968a2b6b44924e1e417c66f8f9b28a
Egor <143845440+Eg0rik@users.noreply.github.com>
2024-03-01 11:56:03 +0000

    Функция для подсчета количества единиц в двоичной записи числа

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 /* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
+
+// Функция для подсчета количества единиц в двоичной записи числа
+int countOnes(int num) {
+    int count = 0;
+    while (num > 0) {
+        count += num % 2;
+        num /= 2;
+    }
+    return count;
+}
 
 void main (void)
 {
