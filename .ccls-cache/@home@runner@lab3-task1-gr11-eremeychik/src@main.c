c5e9e850a61799fccbb979da29ad61a40e8b6fcd
Egor <143845440+Eg0rik@users.noreply.github.com>
2024-03-01 12:11:41 +0000

    functions to other file "dop.c"

--- .ccls-cache/@home@runner@lab3-task1-gr11-eremeychik/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr11-eremeychik/src@main.c
@@ -1,26 +1,6 @@
 /* hello.c */
 #include <stdio.h>
-#include <stdlib.h>
-
-// Функция для подсчета количества единиц в двоичной записи числа
-int countOnes(int num) {
-    int count = 0;
-    while (num > 0) {
-        count += num % 2;
-        num /= 2;
-    }
-    return count;
-}
-
-// Функция для сравнения двух чисел по количеству единиц в их двоичной записи
-int compareByOnes(const void *a, const void *b) {
-    return countOnes(*(int*)a) - countOnes(*(int*)b);
-}
-
-// Функция для генерации псевдослучайных чисел в заданном диапазоне
-int getRandomNumber(int min, int max) {
-    return rand() % (max - min + 1) + min;
-}
+#include "dop.c"
 
 int main(int argc, char *argv[]) {
     // Проверка наличия аргументов командной строки
